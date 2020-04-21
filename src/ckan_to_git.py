46c12036bba3fed5159976593545b6625a53379f
uwaheed88 <usman.waheed@datopian.com>
2020-04-21 15:00:32 -0700

    deleting the lfspointerfile corresponding to resource

--- src/ckan_to_git.py
+++ src/ckan_to_git.py
@@ -6,27 +6,29 @@ from ckanapi.datapackage import dataset_to_datapackage
 log = logging.getLogger(__name__)
 
 class CKANGitClient:
-    def __init__(self, token, pkg_dict=None, entity=None):
+    def __init__(self, token, pkg_dict=None, dataset_name=None, resource_name=None, resources=None):
         g = Github(token)
         self.auth_user = g.get_user()
         self.pkg_dict = pkg_dict
+        self.resource_name = resource_name
+        self.resources = resources
 
         if not pkg_dict:
-            repo_name = entity
+            repo_name = dataset_name
             repo_notes = None
         else:
             repo_name = pkg_dict['name']
             repo_notes = pkg_dict['notes']
 
         self.repo = self.get_or_create_repo(repo_name, repo_notes)
-    
+
     def get_or_create_repo(self, name, notes):
         try:
             repo = self.auth_user.get_repo(name)
-        
+
         except UnknownObjectException as e:
             repo = self.auth_user.create_repo(name, notes)
-        
+
         return repo
 
     def create_datapackage(self):
@@ -60,13 +62,13 @@ class CKANGitClient:
             'Create .lfsconfig',
             '[remote "origin"]\n\tlfsurl = ' + repoUrl
             )
-    
+
     def update_datapackage(self):
         if not self.pkg_dict:
             return
 
         contents = self.repo.get_contents("datapackage.json")
-        body = dataset_to_datapackage(self.pkg_dict)   
+        body = dataset_to_datapackage(self.pkg_dict)
         self.repo.update_file(
             contents.path,
             "Update datapackage.json",
@@ -81,14 +83,14 @@ class CKANGitClient:
         try:
             lfs_pointers = [obj.name for obj in self.repo.get_contents("data")]
             lfs_pointers = {obj:self.get_sha256(obj) for obj in lfs_pointers}
-    
+
         except UnknownObjectException as e:
             lfs_pointers = dict()
-    
+
         for obj in self.pkg_dict['resources']:
             if obj['name'] not in lfs_pointers.keys():
                 self.create_lfspointerfile(obj)
-        
+
             elif obj['sha256'] != lfs_pointers[obj['name']]:
                 self.update_lfspointerfile(obj)
 
@@ -99,7 +101,7 @@ class CKANGitClient:
         sha256 = obj['sha256']
         size = obj['size']
         lfs_pointer_body = 'version https://git-lfs.github.com/spec/v1\noid sha256:{}\nsize {}\n'.format(sha256, size)
-        
+
         self.repo.create_file(
         "data/{}".format(obj['name']),
         "Create LfsPointerFile",
@@ -111,19 +113,35 @@ class CKANGitClient:
         sha256 = obj['sha256']
         size = obj['size']
         lfs_pointer_body = 'version https://git-lfs.github.com/spec/v1\noid sha256:{}\nsize {}\n'.format(sha256, size)
-        
+
         self.repo.update_file(
             contents.path,
             "Update LfsPointerFile",
             lfs_pointer_body,
             contents.sha
             )
-    
+
+    def delete_lfspointerfile(self):
+        contents = self.repo.get_contents("data/{}".format(self.resource_name))
+        self.repo.delete_file(
+            contents.path,
+            "remove lfspointerfile",
+            contents.sha)
+
+    def check_after_delete(self):
+        contents = self.repo.get_contents("data/")
+        if len(self.resources) > len(contents):
+            contents_name = [obj.name for obj in contents]
+
+            for obj in resources:
+                if obj['name'] not in contents_name:
+                    self.create_lfspointerfile(obj)
+
     def delete_repo(self):
         try:
             self.repo.delete()
             log.info("{} repository deleted.".format(self.repo.name))
             return True
-       
+
         except Exception as e:
             return False
